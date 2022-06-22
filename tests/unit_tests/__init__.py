7242b868f4358112f847980e428928623a653141
Utkarsh <utkarshboom77@gmail.com>
2022-06-23 00:32:50 +0800

    update unit_test

--- tests/unit_tests/__init__.py
+++ tests/unit_tests/__init__.py
@@ -2,7 +2,7 @@ import sys
 from os.path import dirname, join, normpath
 
 THIS_DIR = dirname(__file__)
-BIN_PROJ_DIR = normpath(join(THIS_DIR, "..", "..", "src", "binary_class"))
-REG_PROJ_DIR = normpath(join(THIS_DIR, "..", "..", "src", "regression"))
+BIN_PROJ_DIR = normpath(join(THIS_DIR, "..", "..", "src", "feature_analysis", "binary_class"))
+REG_PROJ_DIR = normpath(join(THIS_DIR, "..", "..", "src", "feature_analysis", "regression"))
 sys.path.append(BIN_PROJ_DIR)
 sys.path.append(REG_PROJ_DIR)
